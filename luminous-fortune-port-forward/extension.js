249f52e14284d08275dd63ea7ccc88bdbdb5b31a
SRIDHAR DSCV <sridhardscv@gmail.com>
2023-09-28 10:56:07 +0530

    Port details Input

--- luminous-fortune-port-forward/extension.js
+++ luminous-fortune-port-forward/extension.js
@@ -1,45 +1,81 @@
 const vscode = require("vscode")
+const net = require('net')
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 
-async function activate(context) {
+function tcp_Connection(sourcePort, destinationHost, destinationPort) {
+    const server = net.createServer((sourceSocket) => {
 
-	let disposable = vscode.commands.registerCommand(
-		"luminous-fortune-port-forward.port-forward",
-		async function () {
-			const userInput = await vscode.window.showInputBox({
-				prompt: 'Enter Port Number or Host Name with Port Number : ',
-				placeHolder: 'Enter here...',
-			});
-			if (userInput) {
-				// vscode.window.showInformationMessage(`Value is : ${userInput}`);
-				if(typeof userInput == 'number' && Number(userInput) != NaN){
-					vscode.window.showInformationMessage(`Value type is  Number: ${userInput}`);
-				}
-				else if(userInput == 'string' && userInput.split(":")){
-					const userinput = userInput.split(":");
-					const hostName = userinput[0];
-					const portNumber = userinput[1];
-					// TODO: Add Functionalities
-				}
-				else {
-					vscode.window.sshowErrorMessage(`Given String is invalid : ${userInput}`);
-				}
-			} else {
-				vscode.window.showWarningMessage('Port Forward ==> No input provided...');
-			}
-		}
-	)
+        const destinationSocket = net.connect(destinationPort, destinationHost);
 
-	context.subscriptions.push(disposable)
+        sourceSocket.pipe(destinationSocket);
+        destinationSocket.pipe(sourceSocket);
+
+        sourceSocket.on('close', () => {
+            console.log('Source socket closed');
+            destinationSocket.end();
+        });
+
+        destinationSocket.on('close', () => {
+            console.log('Destination socket closed');
+            sourceSocket.end();
+        });
+    });
+
+    server.listen(sourcePort, () => {
+        vscode.window.showInformationMessage(`TCP server listening on port ${sourcePort} and forwarding to ${destinationHost}:${destinationPort}`);
+    });
+}
+
+function checkInputFormat(inputString, char1, char2) {
+    return inputString.includes(char1) && inputString.includes(char2);
+}
+function activate(context) {
+
+    let disposable = vscode.commands.registerCommand(
+        "luminous-fortune-port-forward.port-forward",
+        async function () {
+            const userInput = await vscode.window.showInputBox({
+                prompt: 'Enter source Port Number or source Host Name with Port Number : ',
+                placeHolder: 'Enter here...',
+            });
+            if ((typeof userInput === "string") && checkInputFormat(userInput, ':', '/')) {
+                // vscode.window.showInformationMessage("Success");
+                const data = userInput.split("/");
+                if (data[1] === 'tcp' || data[1] === 'TCP') {
+                    // vscode.window.showInformationMessage("Tcp Protocal Port Forwrding Methods");
+                    const datas = data[0].split(":");
+                    const hostname = datas[0];
+                    const portname = datas[1];
+                    vscode.window.showInformationMessage(`Port number ${portname},Host name ${hostname}`);
+                    // Port forwarding using TCP Connection
+                    tcp_Connection(portname,hostname,portname);
+                    vscode.window.showInformationMessage(`TCP Connection Success : host -> ${hostname}, Port Number -> ${portname}`)
+
+                }
+                else if (data[1] === 'udp' || data[1] === 'UDP') {
+                    vscode.window.showInformationMessage("UDP Protocal Port Forwrding Methods");
+                }
+                else {
+                    vscode.window.showErrorMessage("Invalid Network method...");
+                }
+
+            } else {
+                vscode.window.showErrorMessage("Invalid Input values...");
+            }
+
+        }
+    )
+
+    context.subscriptions.push(disposable)
 }
 exports.activate = activate
 
 function deactivate() { }
 
 module.exports = {
-	activate,
-	deactivate,
+    activate,
+    deactivate,
 }
